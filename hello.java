c23bea5de5293760186984cda1b2821d2c963c4b
udayhp <udayhp123@gmail.com>
2023-06-15 12:10:38 +0530

    change3

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello
 	public static void main(String[] args)
 	 {
 		 System.out.println("welcome to siddaganga polytechnic");
-		 System.out.println("uday kumar hp");
+		 System.out.println("uday prakash hp");
 	 }
 
 }
